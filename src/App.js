45ce5e189f415ef05ec9d12121ac9a197aaee17b
Gregory Griffin <mr.gregory.griffin@gmail.com>
2018-08-31 07:42:54 +0200

    initial commit first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import logo from './logo.svg';
+import './App.css';
+import Flat from './components/flat';
+import Marker from './components/marker';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+    })
+  }
+  render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    }
+    return (
+      <div className='app'>
+        <div className='main'>
+          <div className='search'>
+          </div>
+          <div className='flats'>
+            {this.state.flats.map((flat) => {
+              return <Flat key={flat.name} flat={flat} />
+            })}
+          </div>
+        </div>
+          <div className='map'>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+            center={center}
+            zoom={11}
+          >
+            {this.state.flats.map((flat => {
+              return <Marker key={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price} />
+              })
+            )}
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
